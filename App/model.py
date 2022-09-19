2c59ed1d47e1c335554a9b87861973c5c480bd0b
JuanJoseTovar <jj.tovara1@uniandes.edu.co>
2022-09-18 23:49:35 -0500

    prueba req 4

--- App/model.py
+++ App/model.py
@@ -260,6 +260,34 @@ def ActorCompare(title1,title2): #Función Auxiliar Requerimiento 3
                 return True
     else:
         return False
+def genero(generos, catalog): #principal req 4 
+    contadortv = 0
+    contadorpelicula = 0
+    generoscu = lt.newlist()
+    for streaming_service in catalog:
+        for title in lt.iterator(catalog[streaming_service]):
+            if (title["listed_in"] != "" ) and (generos in title["listed_in"]):
+                if title["type"] == "Movie":
+                    contadorpelicula += 1
+                else:
+                    contadortv += 1
+                title["streaming_platform"] = streaming_service
+                title["listed_in"] = title["listed_in"].strip()
+                lt.addLast(generoscu,title)
+    merg.sort(generoscu,cmpgen)
+    return generoscu, contadortv, contadorpelicula
+    
+def cmpgen(title1,title2): #aux req 4 
+    if title1["title"] < title2["title"]:
+        return True
+    elif title1["title"] == title2["title"]:
+        if title1["release_year"] < title2["release_year"]:
+            return True
+        elif title1["release_year"] == title2["release_year"]:
+            if title1["director"] < title2["director"]:
+                return True
+    else:
+        return False
 
 
 
